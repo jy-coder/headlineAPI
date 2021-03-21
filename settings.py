6078191a5d61c7145170a9eb8cd4562cc160fe36
laurencjy <cjyclx@gmail.com>
2021-03-21 23:50:38 +0800

    update: local db

--- settings.py
+++ settings.py
@@ -75,13 +75,8 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'headline',
-    'USER': 'myprojectuser',
-    'PASSWORD': 'password',
-    'HOST': 'localhost',
-    'PORT': '',
-    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
